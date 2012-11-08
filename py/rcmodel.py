242a61d29a89d1d58918f4d119ed20c857b9aee1
Jo Bovy <jo.bovy@gmail.com>
2012-11-08 00:02:40 +0000

    rcdist: start on rcmodel

---
+++ py/rcmodel.py
@@ -0,0 +1,303 @@
+###############################################################################
+# rcmodel.py: isochrone model for the distribution in (J-Ks,M_H)
+#             along the RC
+###############################################################################
+import math
+import numpy
+from scipy import maxentropy, integrate
+import scipy.interpolate
+import isodist, isodist.imf
+try:
+    from galpy.util import bovy_plot
+    _BOVY_PLOT_LOADED= True
+except ImportError:
+    _BOVY_PLOT_LOADED= False   
+class rcmodel:
+    """rcmodel: isochrone model for the distribution in (J-Ks,M_H) along the RC"""
+    def __init__(self,imfmodel='lognormalChabrier2001',Z=None,
+                 interpolate=False,expsfh=False,band='H',
+                 dontgather=False,loggmin=None,loggmax=None,
+                 basti=False):
+        """
+        NAME:
+           __init__
+        PURPOSE:
+           initialize rcmodel
+        INPUT:
+           Z= metallicity (if not set, use flat prior in Z over all Z; can be list)
+           imfmodel= (default: 'lognormalChabrier2001') IMF model to use (see isodist.imf code for options)
+           band= band to use for M_X (JHK)
+           interpolate= (default: False) if True, interpolate the binned representation
+           expsfh= if True, use an exponentially-declining star-formation history
+           dontgather= if True, don't gather surrounding Zs
+           loggmin= if set, cut logg at this minimum
+           loggmax= if set, cut logg at this maximum
+           basti= if True, use Basti isochrones (if False, use Padova)
+        OUTPUT:
+           object
+        HISTORY:
+           2012-11-07 - Written - Bovy (IAS)
+        """
+        #Read isochrones
+        if basti:
+            zs= numpy.array([0.0001,0.0003,0.0006,0.001,0.002,0.004,0.008,
+                             0.01,0.0198,0.03,0.04])
+        else:
+            zs= numpy.arange(0.0005,0.03005,0.0005)
+        if Z is None:
+            Zs= zs
+        elif isinstance(Z,float):
+            if basti or dontgather:
+                Zs= [Z]
+            elif Z < 0.001 or Z > 0.0295:
+                Zs= [Z] 
+            elif Z < 0.0015 or Z > 0.029:
+                Zs= [Z-0.0005,Z,Z+0.0005] #build up statistics
+            elif Z < 0.01:
+                Zs= [Z-0.001,Z-0.0005,Z,Z+0.0005,Z+0.001] #build up statistics
+            else:
+                Zs= [Z-0.0005,Z,Z+0.0005] #build up statistics
+        if basti:
+            p= isodist.BastiIsochrone(Z=Zs)
+        else:
+            p= isodist.PadovaIsochrone(Z=Zs)
+        #Get relevant data
+        sample= []
+        weights= []
+        loggs= []
+        for logage in p.logages():
+            for zz in range(len(Zs)):
+                thisiso= p(logage,Zs[zz],asrecarray=True)
+                #Calculate int_IMF for this IMF model
+                if not imfmodel == 'lognormalChabrier2001': #That would be the default
+                    if imfmodel == 'exponentialChabrier2001':
+                        int_IMF= isodist.imf.exponentialChabrier2001(thisiso.M_ini,int=True)
+                    elif imfmodel == 'kroupa2003':
+                        int_IMF= isodist.imf.kroupa2003(thisiso.M_ini,int=True)
+                    elif imfmodel == 'chabrier2003':
+                        int_IMF= isodist.imf.chabrier2003(thisiso.M_ini,int=True)
+                    else:
+                        raise IOError("imfmodel option not understood (non-existing model)")
+                elif basti:
+                    int_IMF= isodist.imf.lognormalChabrier2001(thisiso.M_ini,int=True)
+                else:
+                    int_IMF= thisiso.int_IMF
+                dN= numpy.roll(int_IMF,-1)-int_IMF
+                for ii in range(1,len(int_IMF)-1):
+                    if basti:
+                        JK= 0.972*(thisiso.J[ii]-thisiso.K[ii])-0.011
+                    else:
+                        JK= thisiso.J[ii]-thisiso.Ks[ii]
+                    if band.lower() == 'h':
+                        if basti:
+                            J= JK+thisiso.K[ii]-0.044
+                            H= J-(0.980*(thisiso.J[ii]-thisiso.H[ii])-0.045)
+                        else:
+                            H= thisiso.H[ii]
+                    elif band.lower() == 'j':
+                        if basti:
+                            J= JK+thisiso.K[ii]-0.044
+                        else:
+                            H= thisiso.J[ii]
+                    elif band.lower() == 'k' or band.lower() == 'ks':
+                        if basti:
+                            H= thisiso.K[ii]-0.044
+                        else:
+                            H= thisiso.Ks[ii]
+                    if JK < 0.3 \
+                            or (not loggmax is None and thisiso['logg'][ii] > loggmax) \
+                            or (not loggmin is None and thisiso['logg'][ii] < loggmin):
+                        continue
+                    if dN[ii] > 0.: 
+                        sample.append([JK,H])
+                        loggs.append([thisiso.logg[ii]])
+                        if expsfh:
+                            weights.append(dN[ii]*10**(logage-7.)*numpy.exp((10.**(logage-7.))/800.)) #e.g., Binney (2010)
+                        else:
+                            weights.append(dN[ii]*10**(logage-7.))
+                    else: 
+                        continue #no use in continuing here   
+        #Form array
+        sample= numpy.array(sample)
+        loggs= numpy.array(loggs)
+        weights= numpy.array(weights)
+        self._sample= sample
+        self._weights= weights
+        #Histogram
+        self._jkmin, self._jkmax= 0.3,1.6
+        self._hmin, self._hmax= -11.,2.
+        self._nbins= 26#49
+        self._djk= (self._jkmax-self._jkmin)/float(self._nbins)
+        self._dh= (self._hmax-self._hmin)/float(self._nbins)
+        self._hist, self._edges= numpy.histogramdd(sample,weights=weights,
+                                                   bins=self._nbins,
+                                                   range=[[self._jkmin,self._jkmax],
+                                                          [self._hmin,self._hmax]])
+        #Save
+        self._Zs= Zs
+        self._interpolate= interpolate
+        self._loghist= numpy.log(self._hist)
+        self._loghist[(self._hist == 0.)]= -numpy.finfo(numpy.dtype(numpy.float64)).max
+        if interpolate:
+            #Form histogram grid
+            jks= numpy.linspace(self._jkmin+self._djk/2.,
+                                self._jkmax-self._djk/2.,
+                                self._nbins)
+            hs= numpy.linspace(self._hmin+self._dh/2.,
+                               self._hmax-self._dh/2.,
+                               self._nbins)
+            self._interpolatedhist= scipy.interpolate.RectBivariateSpline(jks,hs,self._hist,
+                                      bbox=[self._jkmin,self._jkmax,
+                                            self._hmin,self._hmax],
+                                      s=0.)
+        return None
+
+    def __call__(self,jk,h):
+        """
+        NAME:
+           __call__
+        PURPOSE:
+           calls 
+        INPUT:
+           -
+        OUTPUT:
+           -
+        HISTORY:
+           2012-11-07 - Skeleton - Bovy (IAS)
+        """
+        return None
+
+    def mean(self,jk):
+        """
+        NAME:
+           mean
+        PURPOSE:
+           return the mean of the M_x distribution at this J-K
+        INPUT:
+           jk - J-Ks
+        OUTPUT:
+           mean
+        HISTORY:
+           2012-11-07 - Written - Bovy (IAS)
+        """
+        return self.moment(jk,1)
+    
+    def sigma(self,jk):
+        """
+        NAME:
+           sigma
+        PURPOSE:
+           return the standard dev of the M_x distribution at this J-K
+        INPUT:
+           jk - J-Ks
+        OUTPUT:
+           standard deviation
+        HISTORY:
+           2012-11-07 - Written - Bovy (IAS)
+        """
+        return numpy.sqrt(self.moment(jk,2)-self.mean(jk)**2.)
+
+    def moment(self,jk,m):
+        """
+        NAME:
+           moment
+        PURPOSE:
+           return the m-th moment of the M_X distribution at this J-K
+        INPUT:
+           jk - J-Ks
+           m - m-th moment
+        OUTPUT:
+           m-th momemtn
+        HISTORY:
+           2012-11-07 - Written - Bovy (IAS)
+        """
+        if self._interpolate:
+            return integrate.quad((lambda x: x**m*self._interpolatedhist(jk,x)),
+                                   -12.,2.)[0]/\
+                                   integrate.quad((lambda x: self._interpolatedhist(jk,x)),
+                                                  -12.,2.)[0]
+        else:
+            raise NotImplementedError("not using interpolation is not implemented at this time")
+            jkbin= numpy.floor((jk-self._jkmin)/self._djk)
+            hbin= numpy.floor((h-self._hmin)/self._dh)
+            if isinstance(jk,numpy.ndarray):
+                out= numpy.zeros(len(jk))-numpy.finfo(numpy.dtype(numpy.float64)).max
+                jkbin= jkbin.astype('int')
+                hbin= hbin.astype('int')
+                indx= (jkbin >= 0.)*(hbin >= 0.)*(jkbin < self._nbins)\
+                    *(hbin < self._nbins)
+                out[indx]= self._loghist[jkbin[indx],hbin[indx]]
+                return out
+            else:
+                jkbin= int(jkbin)
+                hbin= int(hbin)
+                if jkbin < 0 or jkbin >= self._nbins:
+                    return -numpy.finfo(numpy.dtype(numpy.float64)).max
+                if hbin < 0 or hbin >= self._nbins:
+                    return -numpy.finfo(numpy.dtype(numpy.float64)).max
+                return self._loghist[jkbin,hbin]
+    
+    def plot(self,log=False,conditional=False,nbins=None):
+        """
+        NAME:
+           plot
+        PURPOSE:
+           plot the resulting (J-Ks,H) distribution
+        INPUT:
+           log= (default: False) if True, plot log
+           conditional= (default: False) if True, plot conditional distribution
+                        of H given J-Ks
+           nbins= if set, set the number of bins
+        OUTPUT:
+           plot to output device
+        HISTORY:
+           2012-02-17 - Written - Bovy (IAS)
+        """
+        if not _BOVY_PLOT_LOADED:
+            raise ImportError("'galpy.util.bovy_plot' plotting package not found")
+        #Form histogram grid
+        if nbins is None:
+            nbins= self._nbins
+        jks= numpy.linspace(self._jkmin+self._djk/2.,
+                            self._jkmax-self._djk/2.,
+                            nbins)
+        hs= numpy.linspace(self._hmax-self._dh/2.,#we reverse
+                           self._hmin+self._dh/2.,
+                           nbins)
+        plotthis= numpy.zeros((nbins,nbins))
+        for ii in range(nbins):
+            for jj in range(nbins):
+                plotthis[ii,jj]= self(jks[ii],hs[jj])
+        if not log:
+            plotthis= numpy.exp(plotthis)
+        if conditional: #normalize further
+            for ii in range(nbins):
+                plotthis[ii,:]/= numpy.nanmax(plotthis[ii,:])/numpy.nanmax(plotthis)
+        return bovy_plot.bovy_dens2d(plotthis.T,origin='lower',cmap='gist_yarg',
+                                     xrange=[self._jkmin,self._jkmax],
+                                     yrange=[self._hmax,self._hmin],
+                                     aspect=(self._jkmax-self._jkmin)/(self._hmax-self._hmin),
+                                     xlabel=r'$(J-K_s)_0\ [\mathrm{mag}]$',
+                                     ylabel=r'$M_H\ [\mathrm{mag}]$',
+                                     interpolation='nearest')
+    
+    def plot_samples(self):
+        """
+        NAME:
+           plot_samples
+        PURPOSE:
+           plot the samples that the histogramming is based on 
+        INPUT:
+        OUTPUT:
+           plot to output device
+        HISTORY:
+           2012-06-15 - Written - Bovy (IAS)
+        """
+        if not _BOVY_PLOT_LOADED:
+            raise ImportError("'galpy.util.bovy_plot' plotting package not found")
+        return bovy_plot.bovy_plot(self._sample[:,0],self._sample[:,1],'k,',
+                                     xrange=[self._jkmin,self._jkmax],
+                                     yrange=[self._hmax,self._hmin],
+                                     xlabel=r'$(J-K_s)_0\ [\mathrm{mag}]$',
+                                     ylabel=r'$M_H\ [\mathrm{mag}]$')
+    
