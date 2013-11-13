25dd557a6b6a14a912884defa79142ffe998bbf0
Jo Bovy <jo.bovy@gmail.com>
2013-11-13 23:50:49 +0000

    rcdist: add distance histogram

--- py/plot_distancedist.py
+++ py/plot_distancedist.py
@@ -9,6 +9,31 @@ _EXT='ps'
 def plot_distancedist(basesavefilename):
     #First read the sample
     data= apread.rcsample()
+    #Histogram
+    bovy_plot.bovy_print(fig_width=6.)
+    bovy_plot.bovy_hist(data['RC_DIST'],
+                        histtype='step',
+                        color='k',
+                        lw=1.5,
+                        bins=51,
+                        xrange=[0.,10.],
+                        xlabel=r'$\mathrm{distance}\,(\mathrm{kpc})$',
+                        ylabel=r'$\mathrm{Number\ of\ stars}$',zorder=10)
+    ax= pyplot.gca()
+    ax2= ax.twinx()
+    pyplot.sca(ax2)
+    bovy_plot.bovy_hist(data['RC_DIST'],
+                        histtype='step',
+                        cumulative=True,
+                        color='k',
+                        lw=1.,normed=True,
+                        overplot=True,bins=len(data))
+    ax2.set_xlim(0.,10.)
+    ax2.set_ylim(0.,1.)
+    bovy_plot._add_ticks()
+    ax2.set_ylabel(r'$\mathrm{cumulative\ distribution}$')
+    bovy_plot.bovy_end_print(basesavefilename+'_hist.'+_EXT)
+    #RZ
     bovy_plot.bovy_print()
     bovy_plot.bovy_plot(data['RC_GALR'],data['RC_GALZ'],
                         'k.',ms=2.,
