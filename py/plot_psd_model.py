77c516646bc2bc0595c22c75e212b42e48a438f3
Jo Bovy <jo.bovy@gmail.com>
2014-09-25 02:58:22 +0000

    rcdist: further tweaks

--- py/plot_psd_model.py
+++ py/plot_psd_model.py
@@ -49,38 +49,38 @@ def plot_psd_model(plotfilename,type):
         scale= 4.*numpy.pi*220.
         bovy_plot.bovy_print(fig_width=7.,axes_labelsize=20)
         line1= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdcp[1][1:-3]),
-                                   'k-',lw=1.5,
+                                   'k-',lw=2.,
                                    semilogx=True,
                                    xlabel=r'$k\,(\mathrm{kpc}^{-1})$',
                                    ylabel=r'$\sqrt{P_k}\,(\mathrm{km\,s}^{-1})$',
                                    xrange=xrange,
-                                   yrange=[0.,11.9])
+                                   yrange=[0.,11.9],zorder=1)
         line2= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdsp[1][1:-3]),
-                                   'k--',lw=1.5,
-                                   overplot=True)
+                                   'k--',lw=2.,
+                                   overplot=True,zorder=1)
         line3= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdcpsp[1][1:-3]),
-                                   'k-.',lw=1.5,
+                                   'k-.',lw=2.,zorder=1,
                                    overplot=True)
         line4= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdcpp2[1][1:-3]),
-                                   '-',lw=1.5,color='y',
+                                   '-',lw=2.,color='y',zorder=1,
                                    overplot=True)
         line5= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdspp2[1][1:-3]),
-                                   '--',lw=1.5,color='y',
+                                   '--',lw=2.,color='y',zorder=1,
                                    overplot=True)
         line6= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdcpspp2[1][1:-3]),
-                                   '-.',lw=1.5,color='y',
+                                   '-.',lw=2.,color='y',zorder=1,
                                    overplot=True)
         line7= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdcppm3[1][1:-3]),
-                                   '-',lw=1.5,color='r',
+                                   '-',lw=2.,color='r',zorder=1,
                                    overplot=True)
         line8= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdsppm3[1][1:-3]),
-                                   '--',lw=1.5,color='r',
+                                   '--',lw=2.,color='r',zorder=1,
                                    overplot=True)
         line9= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdcpsppm3[1][1:-3]),
-                                   '-.',lw=1.5,color='r',
+                                   '-.',lw=2.,color='r',zorder=1,
                                    overplot=True)
         pyplot.annotate(r'$\mathrm{Elliptical\ perturbation}\ (m=2\ \mathrm{mode})$',
-                        (0.5,1.06),xycoords='axes fraction',
+                        (0.5,1.08),xycoords='axes fraction',
                         horizontalalignment='center',
                         verticalalignment='top',size=20.)
         l1= pyplot.legend((line1[0],line2[0],line3[0]),
@@ -115,17 +115,17 @@ def plot_psd_model(plotfilename,type):
         scale= 4.*numpy.pi*220.
         bovy_plot.bovy_print(fig_width=7.,axes_labelsize=20)
         line1= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdbar[1][1:-3]),
-                                   'r-',lw=1.5,
+                                   'r-',lw=2.,
                                    semilogx=True,
                                    xlabel=r'$k\,(\mathrm{kpc}^{-1})$',
                                    ylabel=r'$\sqrt{P_k}\,(\mathrm{km\,s}^{-1})$',
                                    xrange=xrange,
-                                   yrange=[0.,11.9])
+                                   yrange=[0.,11.9],zorder=1)
         line2= bovy_plot.bovy_plot(ks,scale*numpy.sqrt(psdslowbar[1][1:-3]),
-                                   'y--',lw=1.5,
-                                   overplot=True)
+                                   'y-.',lw=2.,
+                                   overplot=True,zorder=1)
         pyplot.annotate(r'$\mathrm{Bar\ perturbation\ (rotating}\ m=2\ \mathrm{mode})$',
-                        (0.5,1.06),xycoords='axes fraction',
+                        (0.5,1.08),xycoords='axes fraction',
                         horizontalalignment='center',
                         verticalalignment='top',size=20.)
         l1= pyplot.legend((line1[0],line2[0]),
@@ -135,10 +135,59 @@ def plot_psd_model(plotfilename,type):
                           numpoints=8,
                           prop={'size':16},
                           frameon=False)    
+    elif type.lower() == 'spiral':
+        vlosfid= galpy_simulations.vlos('../sim/spiral_rect_omegas0.33_alpha-14.sav')
+        vloslpitch= galpy_simulations.vlos('../sim/spiral_rect_omegas0.33_alpha-7.sav')
+        vlosdiffgamma= galpy_simulations.vlos('../sim/spiral_rect_omegas0.33_gamma0.3.sav')
+        vlosdiffomegas= galpy_simulations.vlos('../sim/spiral_rect_alpha-14.sav')
+        potscale= 0.85
+        eres= 19
+        xgrid= numpy.linspace((_RCXMIN-8.)/8.+_RCDX/8./2.,
+                              (_RCXMAX-8.)/8.-_RCDX/8./2.,
+                              eres)
+        dx= (xgrid[1]-xgrid[0])*8.
+        psdfid= bovy_psd.psd1d(vlosfid,dx,binsize=0.8)
+        psdlpitch= bovy_psd.psd1d(vloslpitch,dx,binsize=0.8)
+        psddiffgamma= bovy_psd.psd1d(vlosdiffgamma,dx,binsize=0.8)
+        psddiffomegas= bovy_psd.psd1d(vlosdiffomegas,dx,binsize=0.8)
+        ks= psdfid[0][1:-3]
+        scale= 4.*numpy.pi*220.
+        bovy_plot.bovy_print(fig_width=7.,axes_labelsize=20)
+        line1= bovy_plot.bovy_plot(ks,potscale*scale*numpy.sqrt(psdfid[1][1:-3]),
+                                   'k-',lw=2,
+                                   semilogx=True,
+                                   xlabel=r'$k\,(\mathrm{kpc}^{-1})$',
+                                   ylabel=r'$\sqrt{P_k}\,(\mathrm{km\,s}^{-1})$',
+                                   xrange=xrange,
+                                   yrange=[0.,11.9],zorder=1)
+        potscale= 0.25
+        line2= bovy_plot.bovy_plot(ks,potscale*scale*numpy.sqrt(psdlpitch[1][1:-3]),
+                                   'y--',lw=2.,zorder=1,
+                                   overplot=True)
+        potscale= 0.4
+        line3= bovy_plot.bovy_plot(ks,potscale*scale*numpy.sqrt(psddiffgamma[1][1:-3]),
+                                   'r:',lw=2.,zorder=1,
+                                   overplot=True)
+        line4= bovy_plot.bovy_plot(ks,4.*scale*numpy.sqrt(psddiffomegas[1][1:-3]),
+                                   'b-.',lw=2.,zorder=1,
+                                   overplot=True)
+        pyplot.annotate(r'$\mathrm{Spiral\ perturbation}$',
+                        (0.5,1.08),xycoords='axes fraction',
+                        horizontalalignment='center',
+                        verticalalignment='top',size=20.)
+        l1= pyplot.legend((line1[0],line2[0],line3[0],line4[0]),
+                          (r'$\mathrm{Fiducial}$',
+                           r'$\mathrm{Pitch\ angle} = 16^\circ$',
+                           r'$\gamma = 17^\circ$',
+                           r'$\Omega_s = 0.65\,\Omega_0$'),
+                          loc='upper right',#bbox_to_anchor=(.91,.375),
+                          numpoints=8,
+                          prop={'size':16},
+                          frameon=False)    
     #Also plot fiducial
     bovy_plot.bovy_plot(tks,
                         scale*numpy.sqrt(simpsd1d[1][1:-3]),
-                        '--',color='0.65',lw=2.,overplot=True)
+                        '-',color='0.65',lw=2.,overplot=True,zorder=0)
     bovy_plot.bovy_end_print(plotfilename)
     return None
 
