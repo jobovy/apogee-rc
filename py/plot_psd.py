0bc294013e0aaa34b59e0ec66974bbcf842d5d4e
Jo Bovy <jo.bovy@gmail.com>
2014-09-25 14:57:03 +0000

    rcdist: more info about the model on the plot

--- py/plot_psd.py
+++ py/plot_psd.py
@@ -149,7 +149,7 @@ def plot_psd(plotfilename):
         #                           scale*numpy.sqrt(simpsd1d[1][1:-3]+4./scale**2.),
         #                           'k-.',lw=2.,overplot=True,dashes=(10,5,3,5))
         pyplot.legend((line1[0],),
-                      (r'$\mathrm{Spiral}:\ \delta \phi_{\mathrm{rms}} = (10\,\mathrm{km\,s}^{-1})^2,$'+'\n'+r'$\mathrm{pitch\ angle} = 8^\circ$',),
+                      (r'$\mathrm{Spiral}:\ \delta \phi_{\mathrm{rms}} = (10\,\mathrm{km\,s}^{-1})^2,$'+'\n'+r'$\mathrm{pitch\ angle} = 8^\circ$'+'\n'+r'$\mathrm{Sun\ near\ 2\!:\!1\ Lindblad\ resonance}$',),
                       loc='upper right',#bbox_to_anchor=(.91,.375),
                       numpoints=8,
                       prop={'size':14},
