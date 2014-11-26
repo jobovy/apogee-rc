e6146e7e9d8edc18479501b3ac5a1200ec751b33
Jo Bovy <jo.bovy@gmail.com>
2014-11-26 22:26:30 +0000

    rcdist: label lines and plot different wave length ranges

--- py/bovy_metallicity_gradient.py
+++ py/bovy_metallicity_gradient.py
@@ -16,7 +16,30 @@ import numpy
 from galpy.util import bovy_plot, save_pickles
 import apogee.tools.read as apread
 _PLOTMAD= False
-def bovy_metallicity_gradient(plotfilename,savefilename):
+line_labels= {}
+line_labels['fe']= r'$\mathrm{Fe\ I}$'
+line_labels['mg']= r'$\mathrm{Mg\ I}$'
+line_labels['al']= r'$\mathrm{Al\ I}$'
+line_labels['si']= r'$\mathrm{Si\ I}$'
+line_labels['k']= r'$\mathrm{K\ I}$'
+line_labels['ca']= r'$\mathrm{Ca\ I}$'
+line_labels['ti']= r'$\mathrm{Ti\ I}$'
+line_labels['cr']= r'$\mathrm{Cr\ I}$'
+line_labels['ni']= r'$\mathrm{Ni\ I}$'
+_FEI_lines= [15198.644,15211.682,15399.925,15494.572,15652.786,15969.229,
+             16045.040,16157.660,16169.448]
+_MGI_lines= [15745.017,15753.203,15770.108,15883.839,15890.541,15893.826,
+             15958.836]
+_ALI_lines= [16723.524,16767.938]
+_SII_lines= [15365.359,15381.033,15837.928,15964.424,16064.397,16099.184,
+             16220.100,16685.327,16832.756]
+_KI_lines= [15167.211,15172.521]
+_CAI_lines= [16141.232,16155.176,16159.650,16161.778]
+_TII_lines= [15548.003,15607.106,15703.269,15719.867,16639.705]
+_CRI_lines= [15684.348,15864.548]
+_NII_lines= [15609.944,15636.926,16588.970,16593.827,16678.266,16820.064,
+             16823.354]
+def bovy_metallicity_gradient(plotfilename,savefilename,largewave=False):
     # First read the RC catalog and cut it to stars near the plane
     data= apread.rcsample()
     indx= (numpy.fabs(data['RC_GALZ']) < 0.25)*(data['METALS'] > -1000.)
@@ -70,7 +93,12 @@ def bovy_metallicity_gradient(plotfilename,savefilename):
             # Normalize by the solar radius
             median_spec[jj,:]/= numpy.median(median_spec[jj,roindx-3:roindx+4])
     # Now plot
-    startindx, endindx= 3652, 4100#3915
+    if False:
+        startindx, endindx= 3652, 4100#3915
+    if largewave:
+        startindx, endindx= 7000,7600#7375, 7889
+    else:
+        startindx, endindx= 2500, 3100
     bovy_plot.bovy_print(fig_width=7.,fig_height=4.)
     bovy_plot.bovy_dens2d((1.-(median_spec[startindx:endindx,:]-1.)).T,
                           origin='lower',cmap='coolwarm',#cmap='afmhot',
@@ -85,8 +113,53 @@ def bovy_metallicity_gradient(plotfilename,savefilename):
     # Draw solar line
     bovy_plot.bovy_plot([-1000000000,100000000],[8.,8.],'k--',lw=1.5,
                         overplot=True)
+    # Label the lines
+    wave= 10.**(numpy.arange(hdr['CRVAL1'],
+                             hdr['CRVAL1']+len(spec)*hdr['CDELT1'],
+                             hdr['CDELT1']))
+    _label_lines('fe',wave[startindx],wave[endindx])
+    _label_lines('mg',wave[startindx],wave[endindx])
+    _label_lines('si',wave[startindx],wave[endindx])
+    _label_lines('al',wave[startindx],wave[endindx])
+    _label_lines('k',wave[startindx],wave[endindx])
+    _label_lines('cr',wave[startindx],wave[endindx])
+    _label_lines('ca',wave[startindx],wave[endindx])
+    _label_lines('ti',wave[startindx],wave[endindx])
+    _label_lines('ni',wave[startindx],wave[endindx])
     bovy_plot.bovy_end_print(plotfilename)
     return None
 
+def _label_lines(elem,wavemin,wavemax):
+    if elem.lower() == 'fe':
+        lines= _FEI_lines
+    elif elem.lower() == 'mg':
+        lines= _MGI_lines
+    elif elem.lower() == 'al':
+        lines= _ALI_lines
+    elif elem.lower() == 'si':
+        lines= _SII_lines
+    elif elem.lower() == 'k':
+        lines= _KI_lines
+    elif elem.lower() == 'cr':
+        lines= _CRI_lines
+    elif elem.lower() == 'ca':
+        lines= _CAI_lines
+    elif elem.lower() == 'ti':
+        lines= _TII_lines
+    elif elem.lower() == 'ni':
+        lines= _NII_lines
+    for line in lines:
+        if line > wavemin and line < wavemax:
+            bovy_plot.bovy_plot([numpy.log10(line)-numpy.log10(15000.),
+                                 numpy.log10(line)-numpy.log10(15000.)],
+                                [12.5,13.],'w-',overplot=True)
+            bovy_plot.bovy_text(numpy.log10(line)-numpy.log10(15000.),
+                                13.1,line_labels[elem.lower()],
+                                size=10.)
+    return None
+
 if __name__ == '__main__':
-    bovy_metallicity_gradient(sys.argv[1],sys.argv[2])
+    if len(sys.argv) > 3:
+        bovy_metallicity_gradient(sys.argv[1],sys.argv[2],largewave=True)
+    else:
+        bovy_metallicity_gradient(sys.argv[1],sys.argv[2])
