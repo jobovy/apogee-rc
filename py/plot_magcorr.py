fc73ab3661d1b8bdd9ecf5ad93fa7f7169159273
Jo Bovy <jo.bovy@gmail.com>
2015-02-11 03:16:15 +0000

    rcdist: plot magcorr with H

--- py/plot_magcorr.py
+++ py/plot_magcorr.py
@@ -2,12 +2,19 @@ import sys
 import numpy
 from galpy.util import bovy_plot
 from matplotlib import pyplot
-def plot_magcorr(plotfilename):
-    corrs= -1.61\
-        -numpy.array([-1.668586,#solar
-                       -1.649471,#avg'ed over Z dist
-                       -1.643215,# exp. SFH
-                       -1.649527])# Kroupa
+def plot_magcorr(plotfilename,h=False):
+    if h:
+        corrs= -1.49\
+            -numpy.array([-1.586390,#solar
+                           -1.571934,#avg'ed over Z dist
+                           -1.566204,# exp. SFH
+                           -1.572732])# Kroupa
+    else:
+        corrs= -1.61\
+            -numpy.array([-1.668586,#solar
+                           -1.649471,#avg'ed over Z dist
+                           -1.643215,# exp. SFH
+                           -1.649527])# Kroupa
     """
         -numpy.array([-1.615557,#solar
                        -1.653314,#avg'ed over Z dist
@@ -23,7 +30,7 @@ def plot_magcorr(plotfilename):
                          axes_labelsize=24.)
     bovy_plot.bovy_plot([1,2,3,4],corrs,'ko',ms=10.,
                         xrange=[0,5],
-                        yrange=[0.,0.08],
+                        yrange=[0.,0.08+0.03*h],
                         ylabel=r'$\mathrm{magnitude\ offset}$')
     #Put labels and rotate them
     pyplot.xticks([1,2,3,4],
@@ -35,4 +42,4 @@ def plot_magcorr(plotfilename):
     bovy_plot.bovy_end_print(plotfilename)
 
 if __name__ == '__main__':
-    plot_magcorr(sys.argv[1])
+    plot_magcorr(sys.argv[1],h=len(sys.argv) > 2)
